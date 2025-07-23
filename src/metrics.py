b68c9ac0e09814652c8e8fa4b7a8ab41f551511c
nagas1226 <nagasyo.1226@gmail.com>
2025-07-23 11:42:14 +0900

    add metrics

---
+++ src/metrics.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def rmse(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """Calculate Root Mean Squared Error."""
+    return np.sqrt(np.mean((y_true - y_pred) ** 2))
+
+
+def mae(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """Calculate Mean Absolute Error."""
+    return np.mean(np.abs(y_true - y_pred))
